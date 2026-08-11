36e00005c073369c126485c3caa7eb6fdac5696b
HelloEaJoe <yizhou.wang.eng@outlook.com>
2026-08-11 12:11:01 +0800

    Update image source in Home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -69,7 +69,7 @@ export default function Home() {
           <div className="flex items-center justify-center w-full order-3">
             <div className="w-[18rem] h-[18rem] sm:w-[24rem] sm:h-[24rem] lg:w-[34rem] lg:h-[34rem] xl:w-[40rem] xl:h-[40rem] flex items-center justify-center overflow-hidden relative bg-transparent">
               <img
-                src="/images/logos/clutch-home2.png"
+                src="/images/logos/challenge-home.png"
                 alt="Clutch 2026"
                 className="h-full w-full object-contain"
               />
